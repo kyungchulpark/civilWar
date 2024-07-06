ff02820609f8ee234a6541102d1bd7a04e63dc3c
BOOK-G0U23C7LVT\pkc09 <parkkc@akiaka.co.kr>
2024-07-06 18:56:49 +0900

    first

---
+++ src/main/java/com/kpl/civilwar/CivilwarApplication.java
@@ -0,0 +1,13 @@
+package com.kpl.civilwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivilwarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CivilwarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kpl/civilwar/CivilwarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpl.civilwar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivilwarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
